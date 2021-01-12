d66cea3a36e22d6e82124ed6b18c0d9a5eda3413
realibi <reallibi@gmail.com>
2021-01-12 10:17:29 +0600

    middleware and RESTful routing added

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,23 +9,22 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
+	// Because Pat matches the "/" path exactly, we can now remove the manual check// Because Pat matches the "/" path exactly, we can now remove the manual check
+	// of r.URL.Path != "/" from this handler.
 	s, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
-	// Use the new render helper.
 	app.render(w, r, "home.page.tmpl", &templateData{
 		Snippets: s,
 	})
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	// Pat doesn't strip the colon from the named capture key, so we need to
+	// get the value of ":id" from the query string instead of "id".
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -39,28 +38,22 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	// Use the new render helper.
 	app.render(w, r, "show.page.tmpl", &templateData{
 		Snippet: s,
 	})
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
-
+	// Checking if the request method is a POST is now superfluous and can be
+	// removed.
 	title := "O snail"
 	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
 	expires := "7"
-
 	err := app.snippets.Insert(title, content, expires)
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
-
+	// Change the redirect to use the new semantic URL style of /snippet/:id
 	http.Redirect(w, r, fmt.Sprintf("/snippet"), http.StatusSeeOther)
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,7 @@ module realibi.com
 go 1.15
 
 require (
-	github.com/go-sql-driver/mysql v1.5.0 // indirect
-	github.com/jackc/pgx v3.6.2+incompatible
+	github.com/bmizerany/pat v0.0.0-20170815010413-6226ea591a40
+	github.com/justinas/alice v1.2.0
 	github.com/lib/pq v1.9.0
-	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
-	golang.org/x/text v0.3.4 // indirect
 )
